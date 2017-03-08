5f84bb1ff95010f8cecb06eaf643341734d33f98
soichit <soichit@uw.edu>
2017-03-08 01:31:40 -0800

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// import { HeaderComponent } from './header/header.component';
+// import { ProjectsComponent } from './projects/projects.component';
+// import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { LoginNewComponent } from './login-new/login-new.component';
+
+
+// const appRoutes: Routes = [
+//   { path: '', redirectTo: 'projects', pathMatch: 'full' },
+//   { path: 'projects', component: ProjectsComponent },
+//   { path: 'about', component: AboutComponent }
+// ];
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login-new', component: LoginNewComponent}
+
+];
+
+
+export const appRoutingProviders: any[] = [
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
