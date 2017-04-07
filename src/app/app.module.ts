16096b1c0e9dcae455bacfa6c8e52fd4d47dc438
soichit <soichit@uw.edu>
2017-04-06 21:05:34 -0700

    add project feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -27,6 +27,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing,
     MaterialModule.forRoot()
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Project } from "./project";
+import { FormGroup, FormControl } from "@angular/forms";
+
 
 @Component({
   selector: 'app-projects',
@@ -9,12 +11,31 @@ import { Project } from "./project";
 })
 export class ProjectsComponent implements OnInit {
   private projects: Project[] = [];
+  myForm: FormGroup;
 
-
-  constructor() { }
+  constructor() {
+    this.myForm = new FormGroup({
+      'name': new FormControl(),
+      'sponsor': new FormControl(),
+      'role': new FormControl(),
+      'description': new FormControl()
+    });
+  }
 
   ngOnInit() {
     //this.projects = new Projects();
-    this.projects.push(new Project("Project 1", "No sponsor", "Project Manager", "This was my first project..."))
+    //this.projects.push(new Project("Example Project 1", "No sponsor", "Project Manager", "This was my first project..."))
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+    console.log(this.myForm.value);
+    console.log(this.myForm.value.name);
+    var name = this.myForm.value.name;
+    var sponsor = this.myForm.value.sponsor;
+    var role = this.myForm.value.role;
+    var description = this.myForm.value.description;
+    this.projects.push(new Project(name, sponsor, role, description));
+    this.myForm.reset();
   }
 }
