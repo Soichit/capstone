46ac1e10b66626b19c26cdb6820a5cfd48c341be
soichit <soichit@uw.edu>
2017-04-12 02:16:06 -0700

    migrated to angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
 import { routing, appRoutingProviders }  from './app.routes';
 import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 
 import { LoginNewComponent } from './login-new/login-new.component';
@@ -30,7 +29,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ReactiveFormsModule,
     HttpModule,
     routing,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    BrowserAnimationsModule
   ],
     providers: [appRoutingProviders],
     bootstrap: [AppComponent]
